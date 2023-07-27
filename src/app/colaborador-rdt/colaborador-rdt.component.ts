1d8484b7dd24e9b37b50b6e4584c63b4b658a855
user <Xil.Breks@gmail.com>
2023-07-27 01:23:35 -0500

    fix: la hora del RDT se coloca ahora dentro de un modal

--- src/app/colaborador-rdt/colaborador-rdt.component.ts
+++ src/app/colaborador-rdt/colaborador-rdt.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { AngularFirestore } from '@angular/fire/compat/firestore';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 
 class ObjRdt {
   public idrdt: string = '';
@@ -22,45 +24,108 @@ class ObjRdt {
 export class ColaboradorRdtComponent {
   idColaborador: any = '';
   lstRDTs: Array<ObjRdt> = [];
+  frmEntrada: FormGroup;
+  frmSalida: FormGroup;
+  lUpdating: boolean = false;
 
-  constructor(private db: AngularFirestore) {
+  constructor(
+    private db: AngularFirestore,
+    private modalService: NgbModal,
+  ) {
     this.idColaborador = localStorage.getItem('idusuario');
     this.getRdts();
+
+    /**********************
+     * INIT FORM HOUR-MIN *
+     **********************/
+    this.frmEntrada = new FormGroup({
+      idrdt: new FormControl(null, Validators.required),
+      shoraingreso: new FormControl(null, Validators.required),
+      sminutoingreso: new FormControl(null, Validators.required),
+    });
+    this.frmSalida = new FormGroup({
+      idrdt: new FormControl(null, Validators.required),
+      shorasalida: new FormControl(null, Validators.required),
+      sminutosalida: new FormControl(null, Validators.required),
+    });
   }
 
   public getRdts(): void {
-    console.log(this.idColaborador);
     this.db
       .collection('rdts', (ref) => {
-        return ref.where('idcolaborador', '==', this.idColaborador);
+        return ref.where('idcolaborador', '==', this.idColaborador).orderBy('dfecha','desc').limit(10);
       })
       .valueChanges()
       .subscribe((val: Array<any>) => {
-        this.lstRDTs = val.reverse();
+        this.lstRDTs = val;
       });
   }
 
-  updateHoraIngreso(objHora: any, idrdt: string): void {
-    this.db.collection('rdts').doc(idrdt).update({
-      shoraingreso: objHora.value
+  updateEntrada(): void {
+    this.lUpdating = true;
+    let idrdt = this.frmEntrada.value['idrdt'];
+
+    this.db
+      .collection('rdts')
+      .doc(idrdt)
+      .update({
+        shoraingreso: this.frmEntrada.value['shoraingreso'],
+        sminutoingreso: this.frmEntrada.value['sminutoingreso'],
+      })
+      .then(()=>{
+        this.frmEntrada.reset();
+      })
+      .catch(()=>{
+        window.alert('ERROR al marcar entrada')
+      })
+      .finally(()=>{
+        this.modalService.dismissAll();
+        this.lUpdating = false;
+      });
+  }
+
+  updateSalida(): void {
+    this.lUpdating = true;
+    let idrdt = this.frmSalida.value['idrdt'];
+
+    this.db
+      .collection('rdts')
+      .doc(idrdt)
+      .update({
+        shorasalida: this.frmSalida.value['shorasalida'],
+        sminutosalida: this.frmSalida.value['sminutosalida'],
+      })
+      .then(()=>{
+        this.frmSalida.reset();
+      })
+      .catch(()=>{
+        window.alert('ERROR al marcar salida')
+      })
+      .finally(()=>{
+        this.modalService.dismissAll();
+        this.lUpdating = false;
+      });
+  }
+
+  public openEntradaModal(idrdt: string, modal: any): void {
+    this.frmEntrada.setValue({
+      idrdt: idrdt,
+      shoraingreso: null,
+      sminutoingreso: null
+    })
+    this.modalService.open(modal, {
+      windowClass: 'modal-sm',
     });
   }
 
-  updateMinutoIngreso(objMinuto: any, idrdt: string): void {
-    this.db.collection('rdts').doc(idrdt).update({
-      sminutoingreso: objMinuto.value
-    });
-  }
-
-  updateHoraSalida(objHora: any, idrdt: string): void {
-    this.db.collection('rdts').doc(idrdt).update({
-      shorasalida: objHora.value
-    });
-  }
-
-  updateMinutoSalida(objMinuto: any, idrdt: string): void {
-    this.db.collection('rdts').doc(idrdt).update({
-      sminutosalida: objMinuto.value
+  public openSalidaModal(idrdt: string, modal: any): void {
+    this.frmSalida.setValue({
+      idrdt: idrdt,
+      shorasalida: null,
+      sminutosalida: null
+    })
+    this.modalService.open(modal, {
+      windowClass: 'modal-sm',
     });
   }
 
