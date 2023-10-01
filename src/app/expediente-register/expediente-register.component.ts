b482d530a4ca45cc1b7c3c7fc770f2a5f78fa032
Tonny <Xil.Breks@gmail.com>
2023-09-30 22:44:36 -0500

    feat: Registrar un expediente ahora toma en cuenta el identificador de la materia

--- src/app/expediente-register/expediente-register.component.ts
+++ src/app/expediente-register/expediente-register.component.ts
@@ -4,12 +4,25 @@ import { AngularFirestore } from '@angular/fire/compat/firestore';
 import { Title } from '@angular/platform-browser';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
 
+class ObjMateria {
+  idmateria: string;
+  smateria: string;
+  sespecialidad: string;
+  constructor(data: any) {
+    this.idmateria = data.idmateria;
+    this.smateria = data.smateria;
+    this.sespecialidad = data.sespecialidad;
+  }
+}
+
 @Component({
   selector: 'app-expediente-register',
   templateUrl: './expediente-register.component.html',
   styleUrls: ['./expediente-register.component.scss']
 })
 export class ExpedienteRegisterComponent {
+  lstMateriasTodos: Array<ObjMateria> = [];
+  lstMaterias: Array<ObjMateria> = [];
   frmExpediente: FormGroup;
   lCreating: boolean = false;
 
@@ -21,22 +34,63 @@ export class ExpedienteRegisterComponent {
     this.titleService.setTitle('Registrar Expediente');
 
     this.frmExpediente = new FormGroup({
-      sorganojuris: new FormControl(null, Validators.required),
       sexpediente: new FormControl(null, Validators.compose([
         Validators.required,
         Validators.pattern(/^\S*$/)
       ])),
-      smateria: new FormControl(null, Validators.required),
       sespecialidad: new FormControl(null, Validators.required),
+      idmateria: new FormControl(null, Validators.required),
+      smateria: new FormControl(null, Validators.required),
+      sorganojuris: new FormControl(null, Validators.required),
       sjuez: new FormControl(null, Validators.required),
       sespecialista: new FormControl(null, Validators.required),
       sdemandante: new FormControl(null, Validators.required),
       sdemandado: new FormControl(null, Validators.required),
       sfechainicio: new FormControl(null, Validators.required),
     });
+
+    this.getMaterias();
   }
 
-  crearExpediente(): void {
+  getMaterias() {
+    let obs = this.db.collection('materias')
+      .valueChanges()
+      .subscribe((res: Array<any>) => {
+        res.forEach(a => {
+          this.lstMateriasTodos.push(
+            new ObjMateria(a)
+          )
+        })
+
+        obs.unsubscribe();
+      });
+  }
+
+  setLstMaterias() {
+    let sespecialidad = this.frmExpediente.controls['sespecialidad'].value;
+    this.frmExpediente.controls['idmateria'].reset();
+
+    this.lstMaterias = this.lstMateriasTodos.filter((a) => {
+      if (a.sespecialidad == sespecialidad) {
+        return true;
+      } else {
+        return false;
+      }
+    })
+  }
+
+  setSMateria() {
+    let idmateria = this.frmExpediente.controls['idmateria'].value;
+    let smateria = '--';
+    this.lstMaterias.forEach((a) => {
+      if(idmateria == a.idmateria) {
+        smateria = a.smateria;
+      }
+    })
+    this.frmExpediente.controls['smateria'].setValue(smateria);
+  }
+
+  crearExpediente() {
     this.lCreating = true;
     let sexpediente = this.frmExpediente.value['sexpediente'];
 
@@ -44,7 +98,17 @@ export class ExpedienteRegisterComponent {
       .collection('expedientes')
       .doc(sexpediente)
       .set({
-        ...this.frmExpediente.value,
+        sexpediente: sexpediente,
+        sespecialidad: this.frmExpediente.controls['sespecialidad'].value,
+        idmateria: this.frmExpediente.controls['idmateria'].value,
+        smateria: this.frmExpediente.controls['smateria'].value,
+        sorganojuris: this.frmExpediente.controls['sorganojuris'].value.trim(),
+        sjuez: this.frmExpediente.controls['sjuez'].value.trim(),
+        sespecialista: this.frmExpediente.controls['sespecialista'].value.trim(),
+        sdemandante: this.frmExpediente.controls['sdemandante'].value.trim(),
+        sdemandado: this.frmExpediente.controls['sdemandado'].value.trim(),
+        sfechainicio: this.frmExpediente.controls['sfechainicio'].value.trim(),
+
         sfechacreacion: new Date().getTime().toString(),
         sfechamodificacion: new Date().getTime().toString(),
         salias: sexpediente.slice(0, 10),
--- src/app/testing/testing.component.ts
+++ src/app/testing/testing.component.ts
@@ -7,4 +7,22 @@ import { Component } from '@angular/core';
 })
 export class TestingComponent {
 
+  getListNoMatch() {
+    return [
+      {
+        "sespecialista": "CARPIO MONTES, ALEXANDRA",
+        "sdemandado": "IMPUTADO: ABRIL GARCIA, MICHAEL WINSTON4",
+        "sorganojuris": "7° JUZGADO DE INV. PREPARATORIA VIOL. C MUJER E IGF MOD. AREQUIPA",
+        "sfechainicio": "x",
+        "sjuez": "X",
+        "smateria": "X",
+        "sfechacreacion": "1692292983420",
+        "salias": "05215-2022",
+        "sespecialidad": "PENAL",
+        "sexpediente": "05215-2022-93-0401-JR-PE-07",
+        "sfechamodificacion": "1692292983420",
+        "sdemandante": "AGRAVIADO: LTAP REPRESENTADA POR LIZ SHIRLEY PINTO HUAMANI"
+      }
+    ]
+  }
 }
