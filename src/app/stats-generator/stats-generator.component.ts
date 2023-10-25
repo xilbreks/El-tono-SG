735c6fa38e51b2fb75df62ca64bc04a4121787f5
Tonny <Xil.Breks@gmail.com>
2023-10-25 15:52:00 -0500

    feat: Ahora existe un menu para generadores

--- src/app/stats-generator/stats-generator.component.ts
+++ src/app/stats-generator/stats-generator.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { AngularFirestore } from '@angular/fire/compat/firestore';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 class ObjTarea {
   idrdt: string = '';
@@ -18,20 +19,23 @@ class ObjTarea {
 export class StatsGeneratorComponent {
   lstUsuarios: Array<any> = [];
   lstTareas: Array<any> = [];
-  sfinicio: any;
-  sffinal: any;
+
+  frmStats: FormGroup;
+  lGenerating: boolean = false;
 
   constructor(
     private db: AngularFirestore,
   ) {
-  }
+    /***********************
+     * INIT FORM EDIT TASK *
+     ***********************/
+    this.frmStats = new FormGroup({
+      sinicio: new FormControl(null, Validators.required),
+      sfinal: new FormControl(null, Validators.required),
+      lstusers: new FormControl(null, Validators.required),
+    });
 
-  setInicio(f: any) {
-    this.sfinicio = f;
-  }
-
-  setFinal(f: any) {
-    this.sffinal = f;
+    this.getUsers();
   }
 
   getUsers() {
@@ -42,101 +46,115 @@ export class StatsGeneratorComponent {
       .valueChanges()
       .subscribe((users: Array<any>) => {
         this.lstUsuarios = users;
-        console.log('Usuarios recuperados exitosamente', users);
+        this.frmStats.controls['lstusers'].setValue('ok')
         obs.unsubscribe();
       });
   }
 
-  getTasks() {
+  generar() {
+    this.lGenerating = true;
+    let sDescripcion = `
+      Estadísticas  
+      desde ${this.frmStats.value['sinicio']}
+      hasta ${this.frmStats.value['sfinal']}
+      -
+      (actualizado al ${(new Date()).toLocaleString()})
+    `;
     let obs = this.db
       .collection('tareas', (ref) => {
-        return ref.where('sfecha', '>=', this.sfinicio).where('sfecha', '<=', this.sffinal);
+        return ref.where('sfecha', '>=', this.frmStats.value['sinicio'])
+          .where('sfecha', '<=', this.frmStats.value['sfinal']);
       })
       .valueChanges()
       .subscribe((tasks: Array<any>) => {
         let lstTareas = tasks.filter((t: any) => {
           let nCodEje = Number(t.ncodeje);
-          if(nCodEje < 100) {
+          if (nCodEje < 100) {
             return true;
           }
           else return false;
         })
         this.lstTareas = lstTareas;
-        console.log('Tareas recuperadas exitosamente', lstTareas);
+
+        /////////////////////////////////////////
+        /////////////////////////////////////////
+        /////////////////////////////////////////
+
+        let lstStats: Array<any> = [];
+        lstStats.push({
+          user: '001',
+          texto: sDescripcion
+        });
+        this.lstUsuarios.forEach(usuario => {
+          let suser = usuario.id;
+          let obj = {
+            user: suser,
+            name: usuario.snombre,
+            color: usuario.scolor,
+            tc: {
+              'nc': 0,
+              'nuevo': 0,
+              'antiguo': 0,
+              'varios': 0
+            },
+            ta: {
+              'nc': 0,
+              'por-expediente': 0,
+              'presencial': 0,
+              'via-celular': 0,
+              'via-internet': 0
+            },
+            tp: {
+              'nc': 0,
+              'laboral': 0,
+              'penal': 0,
+              'civil': 0,
+              'familia': 0,
+              'constitucional': 0,
+              'tramite-adm': 0,
+              'tramite-not': 0,
+              'varios': 0
+            },
+            tt: [
+              0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+              0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+              0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+              0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+              0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+              0, 0, 0, 0
+            ]
+          }
+          this.lstTareas.forEach((tarea: ObjTarea) => {
+            if (tarea.idrdt.slice(11) == suser) {
+              obj.tc[tarea.stipocliente] += 1;
+              obj.ta[tarea.stipoatencion] += 1;
+              obj.tp[tarea.sespecialidad] += 1;
+              let ii = Number(tarea.ncodeje);
+              if (ii <= 54) {
+                obj.tt[ii - 1] += 1;
+              }
+            }
+          })
+          lstStats.push(obj);
+        });
+
+        lstStats.forEach(item => {
+          this.db.collection('estadisticas')
+            .doc(item.user)
+            .set(item)
+            .then(res => {
+              this.lGenerating = false;
+              this.frmStats.controls['sinicio'].reset()
+              this.frmStats.controls['sfinal'].reset()
+            })
+            .catch(err => {
+              console.log(err)
+            });
+        });
+
         obs.unsubscribe();
       });
   }
 
-  generar(texto: any) {
-    let lstStats: Array<any> = [];
-    lstStats.push({
-      user: '001',
-      texto: texto.trim()
-    });
-    this.lstUsuarios.forEach(usuario => {
-      let suser = usuario.id;
-      let obj = {
-        user: suser,
-        name: usuario.snombre,
-        color: usuario.scolor,
-        tc: {
-          'nc': 0,
-          'nuevo': 0,
-          'antiguo': 0,
-          'varios': 0
-        },
-        ta: {
-          'nc': 0,
-          'por-expediente': 0,
-          'presencial': 0,
-          'via-celular': 0,
-          'via-internet': 0
-        },
-        tp: {
-          'nc': 0,
-          'laboral': 0,
-          'penal': 0,
-          'civil': 0,
-          'familia': 0,
-          'constitucional': 0,
-          'tramite-adm': 0,
-          'tramite-not': 0,
-          'varios': 0
-        },
-        tt: [
-          0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-          0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-          0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-          0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-          0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-          0, 0, 0, 0
-        ]
-      }
-      this.lstTareas.forEach((tarea: ObjTarea) => {
-        if (tarea.idrdt.slice(11) == suser) {
-          obj.tc[tarea.stipocliente] += 1;
-          obj.ta[tarea.stipoatencion] += 1;
-          obj.tp[tarea.sespecialidad] += 1;
-          let ii = Number(tarea.ncodeje);
-          if (ii <= 54) {
-            obj.tt[ii - 1] += 1;
-          }
-        }
-      })
-      lstStats.push(obj);
-    })
 
-    lstStats.forEach(item => {
-      this.db.collection('estadisticas')
-        .doc(item.user)
-        .set(item)
-        .then(res => {
-          console.log('exito')
-        })
-        .catch(err => {
-          console.log(err)
-        });
-    })
-
-  }
 }
