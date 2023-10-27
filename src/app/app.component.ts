74030832b20efb19f4a5d067cc522b543a1ab186
Tonny <Xil.Breks@gmail.com>
2023-10-27 17:52:05 -0500

    fix: Se corrige la visibilidad del menu de generadores

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,7 +10,7 @@ import { Router } from '@angular/router';
 })
 export class AppComponent {
   user: any;
-  sversion = '1.1.3';
+  sversion = '1.1.4';
 
   constructor(
     private router: Router,
