6b88d5076c61fd8e7907b8d4e1074e8a8be39808
Yoel Medina <Xil.Breks@gmail.com>
2024-03-06 21:29:05 -0500

    fix: Se crean los inicializan componentes de edicion

--- src/app/expediente-item-edit-code/expediente-item-edit-code.component.ts
+++ src/app/expediente-item-edit-code/expediente-item-edit-code.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, OnChanges, Output, SimpleChanges } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
 
 @Component({
   selector: 'app-expediente-item-edit-code',
   templateUrl: './expediente-item-edit-code.component.html',
   styleUrls: ['./expediente-item-edit-code.component.scss']
 })
-export class ExpedienteItemEditCodeComponent {
+export class ExpedienteItemEditCodeComponent implements OnChanges {
+  @Input('sexpediente') sexpediente: string = '';
 
+  constructor(
+    private db: AngularFirestore,
+  ) {
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+
+  }
 }
--- src/app/expediente-item-edit-match/expediente-item-edit-match.component.ts
+++ src/app/expediente-item-edit-match/expediente-item-edit-match.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, OnChanges, Output, SimpleChanges } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
 
 @Component({
   selector: 'app-expediente-item-edit-match',
   templateUrl: './expediente-item-edit-match.component.html',
   styleUrls: ['./expediente-item-edit-match.component.scss']
 })
-export class ExpedienteItemEditMatchComponent {
+export class ExpedienteItemEditMatchComponent implements OnChanges {
+  @Input('sexpediente') sexpediente: string = '';
+  
+  constructor(
+    private db: AngularFirestore,
+  ) {
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    
+  }
 
 }
