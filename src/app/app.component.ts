4fe7c525a05f8d98b15f04e8e9c1f11dad659c74
Yoel Medina <Xil.Breks@gmail.com>
2024-03-17 13:39:55 -0500

    feat: Se actualiza a la version 17 de angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,7 +10,7 @@ import { Router } from '@angular/router';
 })
 export class AppComponent {
   user: any;
-  sversion = '1.6.12';
+  sversion = '1.7.0';
 
   constructor(
     private router: Router,
