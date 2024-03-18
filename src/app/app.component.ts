8e4476896b96ad0c6427fc4949c5dd88758c72d7
Yoel Medina <Xil.Breks@gmail.com>
2024-03-18 15:53:36 -0500

    feat: Se agrega popperjs la v1 y v2 por siaca

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,7 +10,7 @@ import { Router } from '@angular/router';
 })
 export class AppComponent {
   user: any;
-  sversion = '1.7.1';
+  sversion = '1.7.2';
 
   constructor(
     private router: Router,
--- src/app/testing/testing.component.ts
+++ src/app/testing/testing.component.ts
@@ -24,10 +24,107 @@ export class TestingComponent {
   regexp = /^[0-9]{5}[-][0-9]{4}[-][0-9]{1,2}[-][0-9]{4}[-][A-Z]{2}[-][A-Z]{2}[-][0-9]{2}/;
   fcText: FormControl = new FormControl([]);
 
+  lstColaboradores: Array<{
+    id: string,
+    snombre: string,
+  }> = [];
+
+  lstTareas: Array<{
+    idcolaborador: string,
+    idtarea: string,
+    sespecialidad: string, // civil, penal, laboral...
+    sexpediente: string,
+    shorasatencion: string,
+    sminutosatencion: string,
+    sdemandado: string,
+    sdemandante: string,
+    sfecha: string,
+  }> = [];
+
   constructor(
     private db: AngularFirestore,
     private storage: AngularFireStorage
   ) {
+  }
+
+  getColaboradores() {
+    let obs = this.db.collection('colaboradores', ref => {
+      return ref.where('id', '==', 'anyela-apaza');
+    }).valueChanges()
+      .subscribe((res: any) => {
+        this.lstColaboradores = res;
+
+        console.log('ok colaboradores');
+        obs.unsubscribe();
+      });
+  }
+
+  getTareas() {
+    let obs = this.db.collection('tareas', ref => {
+      return ref
+        .where('sfecha', '>=', '2024-02-01')
+        .where('sfecha', '<=', '2024-02-29')
+        .where('idcolaborador', '==', 'anyela-apaza')
+    })
+      .valueChanges()
+      .subscribe((res: any) => {
+        this.lstTareas = res;
+
+        console.log('ok tareas', res.length)
+        obs.unsubscribe();
+      });
+  }
+
+  analizar() {
+    const workbook = XLSX.utils.book_new();
+
+    this.lstColaboradores.forEach(user => {
+      let lstExpManipulados: Array<{
+        sexpediente: string,
+        sdemandante: string,
+        sdemandado: string,
+        ntoques: number,
+        nminutos: number,
+      }> = [];
+
+      this.lstTareas
+        .filter(tar => tar.idcolaborador == user.id)
+        .forEach(tar => {
+          let lexiste = lstExpManipulados.map(e => e.sexpediente).includes(tar.sexpediente.trim());
+
+          if (lexiste) {
+            // si existe
+            let index = lstExpManipulados.findIndex(e => e.sexpediente == tar.sexpediente);
+            lstExpManipulados[index].ntoques += 1;
+            lstExpManipulados[index].nminutos += Number(tar.shorasatencion) * 60 + Number(tar.sminutosatencion);
+          } else {
+            // no existe
+            lstExpManipulados.push({
+              sexpediente: tar.sexpediente,
+              sdemandante: tar.sdemandante,
+              sdemandado: tar.sdemandado,
+              ntoques: 1,
+              nminutos: Number(tar.shorasatencion) * 60 + Number(tar.sminutosatencion),
+            })
+          }
+        });
+
+      lstExpManipulados.sort((a, b) => {
+        if (a.ntoques < b.ntoques) {
+          return 1;
+        } else {
+          return -1;
+        }
+      })
+
+
+      const worksheet = XLSX.utils.json_to_sheet(lstExpManipulados);
+
+      XLSX.utils.book_append_sheet(workbook, worksheet, user.snombre);
+    });
+
+
+    XLSX.writeFile(workbook, 'Analisis Febrero 2024.xlsx', { compression: true });
   }
 
 }
