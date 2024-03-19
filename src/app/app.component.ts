2ad65b39dc1af11f052b8ad6a0c5016629c487d7
Yoel Medina <Xil.Breks@gmail.com>
2024-03-18 22:40:08 -0500

    feat: Se agrega al proyecto el hosting y las funciones

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,7 +10,7 @@ import { Router } from '@angular/router';
 })
 export class AppComponent {
   user: any;
-  sversion = '1.7.3';
+  sversion = '2.0.0';
 
   constructor(
     private router: Router,
