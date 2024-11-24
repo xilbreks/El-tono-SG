74a33df4feba4875487844ea767de3e851c0efdc
Yoel Medina <Xil.Breks@gmail.com>
2024-11-24 12:48:13 -0500

    Se cambia el titulo al titulo de las audiencias por planner

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,7 +10,7 @@ import { Router } from '@angular/router';
 })
 export class AppComponent {
   user: any;
-  sversion = '2.3.1';
+  sversion = '2.3.2';
 
   constructor(
     private router: Router,
