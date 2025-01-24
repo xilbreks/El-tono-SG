b8a3142250fff6424f59091360431b24ba9193d8
Yoel Medina <Xil.Breks@gmail.com>
2025-01-24 11:49:15 -0500

    feat: Se cambia el formato de fecha en las audiencias

--- src/app/trials/trials.component.ts
+++ src/app/trials/trials.component.ts
@@ -2,8 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import { AngularFirestore } from '@angular/fire/compat/firestore';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { firstValueFrom } from 'rxjs';
 
-interface Audience {
+interface Trial {
   idaudiencia: string;
   sexpediente: string;
   sespecialidad: string;
@@ -20,6 +21,10 @@ interface Audience {
   sprefijolink: string;
   ssufijolink: string;
   scuerpolink: string;
+  nombreDia: string;      // Dia de la semana, ej: "Lunes", "Martes", ...
+  nombreMes: string;      // Mes del año, ej: "Enero", "Febrero", ...
+  numeroDia: string;      // Numero del dia del mes
+  numeroAnio: string;     // Numero de año
 }
 
 @Component({
@@ -28,11 +33,11 @@ interface Audience {
   styleUrl: './trials.component.scss'
 })
 export class TrialsComponent implements OnInit {
-  lstAudiencias: Audience[] = [];
-  lstAudLaboral: Audience[] = [];
-  lstAudFamilia: Audience[] = [];
-  lstAudCivil: Audience[] = [];
-  lstAudPenal: Audience[] = [];
+  lstAudiencias: Trial[] = [];
+  lstAudLaboral: Trial[] = [];
+  lstAudFamilia: Trial[] = [];
+  lstAudCivil: Trial[] = [];
+  lstAudPenal: Trial[] = [];
   lLoading = false;
 
   frmDate: FormGroup;
@@ -90,63 +95,93 @@ export class TrialsComponent implements OnInit {
     this.getAudiencias();
   }
 
-  getAudiencias() {
+  async getAudiencias() {
     this.lLoading = true;
     this.lstAudiencias = [];
-    let sinicio = this.frmDate.controls['sinicio'].value;
-    let sfinal = this.frmDate.controls['sfinal'].value;
+    let inicio = this.frmDate.controls['sinicio'].value;
+    let final = this.frmDate.controls['sfinal'].value;
 
-    let obs = this.db.collection('audiencias', ref => {
-      return ref.where('sfecha', '>=', sinicio)
-        .where('sfecha', '<=', sfinal)
-    }).valueChanges()
-      .subscribe((res: Array<any>) => {
-        this.lstAudiencias = res.map(aud => {
-          // Fecha mas legible
-          let sDay = aud.sfecha.slice(8, 10);
-          let sMonth = aud.sfecha.slice(5, 7);
-          let sYear = aud.sfecha.slice(0, 4);
+    let audiencias = await this.getTrialsDB(inicio, final);
 
-          // Detecion de enlace meet en el url
-          const regexMeet = /meet\.google\.com\/[a-z]{3}-{0,1}[a-z]{4}-{0,1}[a-z]{3}/i;
-          let prefijo = aud.surl;
-          let cuerpo = '';
-          let sufijo = '';
-          const texto: string = aud.surl;
-          const enlace: RegExpMatchArray | null = texto.match(regexMeet);
+    this.lstAudiencias = audiencias.map(aud => {
+      // Fecha mas legible
+      let sDay = aud.sfecha.slice(8, 10);
+      let sMonth = aud.sfecha.slice(5, 7);
+      let sYear = aud.sfecha.slice(0, 4);
 
-          // Existe link
-          if (enlace) {
-            let indiceInicio: number = texto.indexOf(enlace[0])
+      // Detecion de enlace meet en el url
+      const regexMeet = /meet\.google\.com\/[a-z]{3}-{0,1}[a-z]{4}-{0,1}[a-z]{3}/i;
+      let prefijo = aud.surl;
+      let cuerpo = '';
+      let sufijo = '';
+      const texto: string = aud.surl;
+      const enlace: RegExpMatchArray | null = texto.match(regexMeet);
 
-            prefijo = texto.slice(0, indiceInicio);
-            cuerpo = texto.slice(indiceInicio, indiceInicio + 28).toLowerCase();
-            sufijo = texto.slice(indiceInicio + 29);
-          }
+      // Existe link
+      if (enlace) {
+        let indiceInicio: number = texto.indexOf(enlace[0])
 
-          return {
-            ...aud,
-            sfechauser: sDay + '/' + sMonth + '/' + sYear,
-            sprefijolink: prefijo,
-            scuerpolink: cuerpo,
-            ssufijolink: sufijo,
-          }
-        }).sort((a, b) => {
-          let sfecha1 = a.sfecha + '-' + a.shora;
-          let sfecha2 = b.sfecha + '-' + b.shora;
-          return sfecha1 < sfecha2 ? -1 : 1;
-        });
+        prefijo = texto.slice(0, indiceInicio);
+        cuerpo = texto.slice(indiceInicio, indiceInicio + 28).toLowerCase();
+        sufijo = texto.slice(indiceInicio + 29);
+      }
 
-        // Crear las lista filtrando segun area de la audiencia
-        this.lstAudLaboral = this.lstAudiencias.filter(a => a.sespecialidad == 'LABORAL' || a.sespecialidad == 'CONSTITUCIONAL');
-        this.lstAudFamilia = this.lstAudiencias.filter(a => a.sespecialidad == 'FAMILIA');
-        this.lstAudCivil = this.lstAudiencias.filter(a => a.sespecialidad == 'CIVIL');
-        this.lstAudPenal = this.lstAudiencias.filter(a => a.sespecialidad == 'PENAL');
+      // Dia de la semana y mes del año
+      let dias = ['Domingo', 'Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes', 'Sábado'];
+      let meses = ['enero', 'febrero', 'marzo', 'abril', 'mayo', 'junio', 'julio', 'agosto', 'septiembre', 'octubre', 'noviembre', 'diciembre']
+      let fecha = new Date(`${aud.sfecha}T00:00`);
+      let numeroDiaSemana = fecha.getUTCDay();
+      let numeroMes = fecha.getMonth();
+      let nombreDia = dias[numeroDiaSemana];
+      let nombreMes = meses[numeroMes];
+      let numeroDia = fecha.getDate();
+      let numeroAnio = fecha.getFullYear();
 
-        this.lLoading = false;
-        obs.unsubscribe();
-      })
+      return {
+        ...aud,
+        sfechauser: sDay + '/' + sMonth + '/' + sYear,
+        sprefijolink: prefijo,
+        scuerpolink: cuerpo,
+        ssufijolink: sufijo,
+        nombreDia,
+        nombreMes,
+        numeroDia: sDay,
+        numeroAnio,
+      }
+    }).sort((a, b) => {
+      let sfecha1 = a.sfecha + '-' + a.shora;
+      let sfecha2 = b.sfecha + '-' + b.shora;
+      return sfecha1 < sfecha2 ? -1 : 1;
+    });
+
+    // Crear las lista filtrando segun area de la audiencia
+    this.lstAudLaboral = this.lstAudiencias.filter(a => a.sespecialidad == 'LABORAL' || a.sespecialidad == 'CONSTITUCIONAL');
+    this.lstAudFamilia = this.lstAudiencias.filter(a => a.sespecialidad == 'FAMILIA');
+    this.lstAudCivil = this.lstAudiencias.filter(a => a.sespecialidad == 'CIVIL');
+    this.lstAudPenal = this.lstAudiencias.filter(a => a.sespecialidad == 'PENAL');
+
+    this.lLoading = false;
   }
 
+  // Operaciones a la base de datos
+
+  /**
+   * Obtiene el listado de audiencias de una fecha determinada, mes
+   * @param inicio primer dia del mes
+   * @param final ultimo dia del mes
+   */
+  getTrialsDB(inicio: string, final: string): Promise<any[]> {
+    let obs = this.db.collection('audiencias', ref => {
+      return ref.where('sfecha', '>=', inicio)
+        .where('sfecha', '<=', final)
+    }).get();
+
+    return firstValueFrom(obs).then(snapshot => {
+      let audiencias: any[] = [];
+      snapshot.forEach(doc => audiencias.push(doc.data()));
+
+      return audiencias;
+    });
+  }
 
 }
