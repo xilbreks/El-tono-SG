585708a88f076247f1adf8860265ef7cf2742854
Yoel Medina <Xil.Breks@gmail.com>
2025-03-25 17:24:18 -0500

    fix: Se corrige el error de la visualizacion de RDT del colaborador

--- src/app/colaborador-rdt/colaborador-rdt.component.ts
+++ src/app/colaborador-rdt/colaborador-rdt.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { AngularFirestore } from '@angular/fire/compat/firestore';
+import { firstValueFrom } from 'rxjs';
 
 class ObjUser {
   id: string = '';
@@ -37,44 +38,57 @@ export class ColaboradorRdtComponent {
   }
 
   getUser() {
-    this.db
-      .collection('colaboradores')
-      .doc(this.idColaborador)
-      .valueChanges()
-      .subscribe((u: any) => {
-        this.objUser.id = u.id;
-        this.objUser.snombre = u.snombre.toUpperCase();
-      })
+    let obs = this.db
+      .collection('colaboradores', ref => {
+        return ref.where('id', '==', this.idColaborador)
+      }).get();
+
+    firstValueFrom(obs).then(snapshot => {
+      let lista: any[] = [];
+      snapshot.forEach(doc => {
+        lista.push(doc.data())
+      });
+
+      const usuario = lista[0];
+      this.objUser.id = usuario.id;
+      this.objUser.snombre = usuario.snombre.toUpperCase();
+    });
   }
 
   getRdts() {
     this.lLoading = true;
-    this.db
+    let obs = this.db
       .collection('rdts', (ref) => {
         return ref.where('idcolaborador', '==', this.idColaborador)
           .orderBy('sfecha', 'desc')
-          .limit(22);
-      })
-      .valueChanges()
-      .subscribe((data: Array<any>) => {
-        this.lstRDTs = data.map((x: any) => {
-          let sday = x.sfecha.slice(8, 10);
-          let smonth = x.sfecha.slice(5, 7);
-          let syear = x.sfecha.slice(0, 4);
-          let sfechalocal = sday + '/' + smonth + '/' + syear;
+          .limit(26);
+      }).get();
 
-          return {
-            idrdt: x.idrdt,
-            sfecha: sfechalocal,
-            leditable: x.leditable,
-            shoraingreso: x.shoraingreso,
-            shorasalida: x.shorasalida,
-            sminutoingreso: x.sminutoingreso,
-            sminutosalida: x.sminutosalida,
-          }
-        });
-        this.lLoading = false;
+    firstValueFrom(obs).then(snapshot => {
+      let lista: any[] = [];
+      snapshot.forEach(doc => {
+        lista.push(doc.data())
       });
+      return lista
+    }).then((data: Array<any>) => {
+      this.lstRDTs = data.map((x: any) => {
+        let sday = x.sfecha.slice(8, 10);
+        let smonth = x.sfecha.slice(5, 7);
+        let syear = x.sfecha.slice(0, 4);
+        let sfechalocal = sday + '/' + smonth + '/' + syear;
+
+        return {
+          idrdt: x.idrdt,
+          sfecha: sfechalocal,
+          leditable: x.leditable,
+          shoraingreso: x.shoraingreso,
+          shorasalida: x.shorasalida,
+          sminutoingreso: x.sminutoingreso,
+          sminutosalida: x.sminutosalida,
+        }
+      });
+      this.lLoading = false;
+    });
   }
 
 }
