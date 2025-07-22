d950b962bf6b5737341cd2c4fd6071e1ea00a503
Yoel Medina <Xil.Breks@gmail.com>
2025-07-22 11:47:42 -0500

    feat: El boton de editar se esconde dinamicamente al pasar el cursor o no

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,7 +10,7 @@ import { Router } from '@angular/router';
 })
 export class AppComponent {
   user: any;
-  sversion = '2.7.9';
+  sversion = '2.7.10';
 
   lstTareasPenal = [
     'Simple: Impulso de procesos con seguimiento de casos en poder judicial o fiscalia',
