961e847c3766c95f8191ce9be05eaa4956a04c4e
Yoel Medina <Xil.Breks@gmail.com>
2025-09-18 12:27:38 -0500

    feat: Se actualiza los tickets al sprint septiembre B

--- src/app/tickets-me/tickets-me.component.ts
+++ src/app/tickets-me/tickets-me.component.ts
@@ -41,7 +41,7 @@ export class TicketsMeComponent {
     // let idSprint = this.fcSprintId.value;
     let obs = this.db.collection('tickets', ref => {
       return ref.where('asignadoA', '==', this.idusuario)
-        .where('idSprint', '==', 'S0009')
+        .where('idSprint', '==', 'S0010')
     }).get();
 
     firstValueFrom(obs).then(snapshot => {
