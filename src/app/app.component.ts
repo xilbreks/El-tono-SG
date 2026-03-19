f5890b37a9379cb4a336522a10f3491a70fd1ce8
Yoel Medina <Xil.Breks@gmail.com>
2026-03-19 10:43:41 -0500

    feat: se da acceso al informe economico a ciertos usuarios

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,7 +10,7 @@ import { Router } from '@angular/router';
 })
 export class AppComponent {
   user: any;
-  sversion = '3.0.16';
+  sversion = '3.0.17';
 
   constructor(
     private router: Router,
