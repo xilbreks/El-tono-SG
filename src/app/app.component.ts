d2a86c6b997673bf4a457d27d6829cb71ea7c1cd
Yoel Medina <Xil.Breks@gmail.com>
2026-04-16 16:30:50 -0500

    feat: se quita permiso a jackeline y se otorga a tuareg

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,7 +10,7 @@ import { Router } from '@angular/router';
 })
 export class AppComponent {
   user: any;
-  sversion = '3.1.2';
+  sversion = '3.1.3';
 
   constructor(
     private router: Router,
